7f945ffe44fc11f35b0f8014e8fb03a6fa47be05
Kevin Harrison <kharrison7@elon.edu>
2024-09-11 10:47:33 -0400

    JRS-9199 add res on get

--- app.js
+++ app.js
@@ -2,16 +2,53 @@ const express = require('express');
 const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
+const cors = require('cors');
 
 // setup based on: https://youtu.be/FTNKDgN4CGI?t=244
 const version = '1.0.0';
 
-app.get('/', (req, res) => {
-    // set response content    
-    res.sendFile(__dirname + "/html/index.html"); 
-    console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
+// app.get('/', (req, res) => {
+//     // set response content    
+//     res.sendFile(__dirname + "/html/index.html"); 
+//     console.log(`[Version ${version}]: New request => http://${hostname}:${port}`+req.url);
 
-})
+// })
+
+
+// 👇️ CORS https://bobbyhadz.com/blog/react-axios-network-error-stack-trace
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // for register local running endpoint
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+    next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  return res.status(200).send({
+    value: 'test get value',
+    message: 'Hello World Updated!',
+  });
+});
 
 // Health check
 app.get('/health', (req, res) => {    
